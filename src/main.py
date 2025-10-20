47542b5bde7e41b53160485e8bf1f269ed78b319
jackkaiser1 <jackkaiser78@gmail.com>
2025-10-20 15:00:04 -0400

    Added .gitignore, folder structure, and started scripting

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# print("hello world")
+
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("hello world", TextType.BOLD, "https://www.boot.dev")
+    print(node.__repr__())
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum 
+
+class TextType(Enum):
+    PLAIN = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "link"
+    IMAGES = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, node_1, node_2):
+        if (node_1.text == node_2.text
+            and node_1.text_type == node_2.text_type
+            and node_1.url == node_2.url):
+            return True
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        
