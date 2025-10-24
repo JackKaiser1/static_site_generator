3c56391880de9c428de864aad311874e65b49e95
jackkaiser1 <jackkaiser78@gmail.com>
2025-10-24 14:39:44 -0400

    Added block_to_block_type func and test suite

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from collections import Counter
 
 
 class BlockType(Enum):
@@ -8,4 +9,59 @@ class BlockType(Enum):
     QUOTE = "quote"
     UNORDERED_LIST = "unordered list"
     ORDERED_LIST = "ordered list"
+
+
+def block_to_block_type(block):
+    pound_count = 0
+
+    for i in range(0, len(block)):
+        char = block[i]
+        next_char = block[i + 1]
+        if char == "#":
+            pound_count += 1
+        if next_char != "#":
+            break
+    if (pound_count >= 1 
+        and pound_count < 7 
+        and next_char == " "):
+        return BlockType.HEADING
+    
+    newline_block = block.split("\n")
+    quote_symbol = 0
+    dash_symbol = 0
+    ordered_num = 1
+    ordered_num_count = 0
+
+    for newline in newline_block:
+        if newline.startswith(">"):
+            quote_symbol += 1
+        if newline.startswith("- "):
+            dash_symbol += 1
+        if newline.startswith(f"{ordered_num}. "):
+            ordered_num_count += 1
+            ordered_num += 1
+    if quote_symbol == len(newline_block):
+        return BlockType.QUOTE      
+    if dash_symbol == len(newline_block):
+        return BlockType.UNORDERED_LIST
+    if ordered_num_count == len(newline_block):
+        return BlockType.ORDERED_LIST
+    
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    if block.startswith("\n> "):
+        return BlockType.QUOTE
+    
+    else:
+        return BlockType.PARAGRAPH
+    
+
+# print(block_to_block_type(
+
+# """1. Codeblock
+# 2. Codeblock
+# 3. Codeblock 
+# 4. Codeblock"""))
+
 
---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,73 @@
+import unittest
+
+from markdown_blocks import BlockType, block_to_block_type
+
+class TestMarkdownBlocks(unittest.TestCase):
+
+    def test_paragraph_to_blocktype(self):
+        paragraph = "This is some plain text"
+        self.assertEqual(block_to_block_type(paragraph), BlockType.PARAGRAPH)
+
+    def test_fail_paragraph_to_blocktype(self):
+        paragraph = "## This is actually a heading"
+        self.assertNotEqual(block_to_block_type(paragraph), BlockType.PARAGRAPH)
+
+    def test_heading_to_blocktype(self):
+        heading = "###### This is a heading"
+        self.assertEqual(block_to_block_type(heading), BlockType.HEADING)
+
+    def test_fail_heading_to_blocktype(self):
+        heading = "######### This is not a heading"
+        self.assertNotEqual(block_to_block_type(heading), BlockType.HEADING)
+
+    def test_code_to_blocktype(self):
+        code = "```This is a code block```"
+        self.assertEqual(block_to_block_type(code), BlockType.CODE)
+
+    def test_fail_code_to_blocktype(self):
+        code = "This is not a code block```"
+        self.assertNotEqual(block_to_block_type(code), BlockType.CODE)
+
+    def test_quote_to_blocktype(self):
+        quote = (
+"""> This is a quote line
+> this is another quote line
+> this is a third quote line""")
+        self.assertEqual(block_to_block_type(quote), BlockType.QUOTE)
+
+    def test_fail_quote_to_blocktype(self):
+        quote = (
+"""> This is a quote line
+this is not a quote line
+> this is a third quote line""")
+        self.assertNotEqual(block_to_block_type(quote), BlockType.QUOTE)
+
+    def test_unordered_to_blocktype(self):
+        unordered_list = (
+"""- Here is an item
+- here is another item 
+- here is a third item""")
+        self.assertEqual(block_to_block_type(unordered_list), BlockType.UNORDERED_LIST)
+
+    def test_fail_unordered_to_blocktype(self):
+        unordered_list = (
+"""Here is an item
+- here is another item 
+- here is a third item""")
+        self.assertNotEqual(block_to_block_type(unordered_list), BlockType.UNORDERED_LIST)
+
+    def test_ordered_to_blocktype(self):
+        ordered_list = (
+"""1. Item 
+2. Item
+3. Item""")
+        self.assertEqual(block_to_block_type(ordered_list), BlockType.ORDERED_LIST)
+
+    def test_fail_ordered_to_blocktype(self):
+        ordered_list = (
+"""1. Item 
+Item
+3. Item""")
+        self.assertNotEqual(block_to_block_type(ordered_list), BlockType.ORDERED_LIST)
+
+    
