4c1c24a549ef0071f5a27fe5b7036bf17e49b9fb
jackkaiser1 <jackkaiser78@gmail.com>
2025-10-29 13:24:29 -0400

    finished markdown_to_html_node and added tests

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -3,6 +3,7 @@ from .htmlnode import HTMLNode, LeafNode, ParentNode
 from .text_to_node import text_to_textnodes, text_node_to_html_node
 from .textnode import TextNode
 from .type_enums import BlockType, TextType
+import re
 
 
 def markdown_to_html_node(markdown) -> ParentNode:
@@ -10,7 +11,7 @@ def markdown_to_html_node(markdown) -> ParentNode:
     return htmlnode_from_block(blocks_list)
 
 
-def htmlnode_from_block(blocks_list):
+def htmlnode_from_block(blocks_list) -> str:
     all_children = []
     for block in blocks_list:
         result = block_to_block_type(block)
@@ -19,28 +20,21 @@ def htmlnode_from_block(blocks_list):
         else:
             block_type = result
 
+        if block_type != BlockType.CODE:
+            block = " ".join(block.split())
+            children = text_to_children(block)
 
-
-        # if block_type == BlockType.HEADING:
-        #     heading_num = block_to_block_type(block)
 
         if block_type == BlockType.CODE:
             block = "".join(block.split("```"))
             block = block.removeprefix("\n")
-        if block_type != BlockType.CODE:
-            block = " ".join(block.split())
-            children = text_to_children(block)
         if block_type == BlockType.HEADING:
             block = block.removeprefix("#" * heading_num + " ")
         if block_type == BlockType.QUOTE:
             block = " ".join(block.split())
             block = "\n".join(block.split(">"))
             block = block.removeprefix("\n")
-        if block_type == BlockType.UNORDERED_LIST:
-            block = " ".join(block.split("- "))
 
-
-            # print(block)
 
         if block_type == BlockType.PARAGRAPH:
             if children == None:
@@ -65,16 +59,10 @@ def htmlnode_from_block(blocks_list):
                 all_children.append(ParentNode("blockquote", children))
 
         elif block_type == BlockType.UNORDERED_LIST:
-            if children == None:
-                all_children.append(LeafNode("ul", block))
-            else:
-                all_children.append(ParentNode("ul", children))
+            all_children.append(list_blocks(block_type, block))
 
         elif block_type == BlockType.ORDERED_LIST:
-            if children == None:
-                all_children.append(LeafNode("ol", block))
-            else:
-                all_children.append(ParentNode("ol", children))
+            all_children.append(list_blocks(block_type, block))
 
     return ParentNode("div", all_children)
 
@@ -89,16 +77,25 @@ def text_to_children(text):
     return child_list
 
 
+def list_blocks(block_type, block):
+    if block_type == BlockType.UNORDERED_LIST:
+        parent_tag = "ul"
+        list_items = block.split("- ")
+    elif block_type == BlockType.ORDERED_LIST:
+        parent_tag = "ol"
+        list_items = re.split(r"\d+\.\s", block)
+
+    list_children = []
+    for item in list_items:
+        if item != "":
+            item = item.strip()
+            item_children = text_to_children(item)
+            if not item_children:
+                list_children.append(LeafNode("li", item))
+            else:
+                list_children.append(ParentNode("li", item_children))
+
+    return ParentNode(parent_tag, list_children)
     
 
 
-# md = """
-# This is **bolded** paragraph
-# text in a p
-# tag here
-
-# This is another paragraph with _italic_ text and `code` here
-
-# """
-# html = markdown_to_html_node(md)
-# print(html.to_html())
--- test/test_markdown_to_html_node.py
+++ test/test_markdown_to_html_node.py
@@ -64,16 +64,68 @@ This is a **bold** paragraph
             "<div><blockquote>This is the first line \nSecond line \nThird line</blockquote></div>"
         )
 
-#     def test_unordered_list(self):
-#         md = """
-# - This is the first line
-# - Second line
-# - Third line"""
+    def test_unordered_list(self):
+        md = """
+- This is the first line
+- Second line
+- Third line"""
 
-#         node = markdown_to_html_node(md)
-#         html = node.to_html()
-#         # print(html)
-#         self.assertEqual(
-#             html,
-#             "<div><blockquote>This is the first line \nSecond line \nThird line</blockquote></div>"
-#         )
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is the first line</li><li>Second line</li><li>Third line</li></ul></div>"
+        )
+
+    def test_unordered_list_children(self):
+        md = """
+- This is the **first** line
+- Second line
+- Third line"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is the <b>first</b> line</li><li>Second line</li><li>Third line</li></ul></div>"
+        )
+
+    def test_ordered_list(self):
+        md = """
+1. This is the first line
+2. Second line
+3. Third line"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>This is the first line</li><li>Second line</li><li>Third line</li></ol></div>"
+        )
+
+    def test_ordered_list__children(self):
+        md = """
+1. This is the **first** line
+2. Second line
+3. Third line"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>This is the <b>first</b> line</li><li>Second line</li><li>Third line</li></ol></div>"
+        )
+
+    def test_heading_ordered_list(self):
+        md = """## Steps to Create a Markdown File
+
+1. Open your favorite text editor.  
+2. Write your content using Markdown syntax.  
+3. Save it with the `.md` extension.  
+4. Preview or convert it to HTML using a Markdown viewer."""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h2>Steps to Create a Markdown File</h2><ol><li>Open your favorite text editor.</li><li>Write your content using Markdown syntax.</li><li>Save it with the <code>.md</code> extension.</li><li>Preview or convert it to HTML using a Markdown viewer.</li></ol></div>"
+        )
